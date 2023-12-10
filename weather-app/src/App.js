706559581c0237504e2ab5ca2fc770335d362838
= <kaweesha.mr@gmail.com>
2023-12-10 21:20:29 +0530

    Update title in index.html and add WeatherApp component in App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import WeatherApp from './components/WeatherApp/WeatherApp';
 
 function App() {
   return (
     <div className="App">
+      <WeatherApp />
     </div>
   );
 }
