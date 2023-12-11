60f0a2c6c13ae7a4a48ab104794cdc64329beffd
= <kaweesha.mr@gmail.com>
2023-12-11 12:13:32 +0530

    Refactor code for improved readability and styling

--- blog-site-mini/src/App.js
+++ blog-site-mini/src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
 
 
-    <Router>    
+    <Router>
       <div className="App">
 
         <h1 style={
           {
-            textAlign:"center",
-            padding:"10px",
-            backgroundColor:"black",
-            color:"white",
+            textAlign: "center",
+            padding: "10px",
+            backgroundColor: "black",
+            color: "white",
+            
           }} > Blog Website</h1>
-        
+
         <Navbar />
-        
-      
+        <Routes>
+        <Route path="/" element={<Home />} />
 
+        </Routes>
 
-    </div>
+      </div>
     </Router>
   );
 }
--- blog-site-mini/src/components/Home.js
+++ blog-site-mini/src/components/Home.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
-
 import { Link } from "react-router-dom";
+import '../styles/Home.css';
 
 const Home = () => {
-    
-    const [post,setPost] = useState([
-        {id:1,title:"Post 1",body:"This is post 1"},
-        {id:2,title:"Post 2",body:"This is post 2"},
-        {id:3,title:"Post 3",body:"This is post 3"},
+
+    const [post, setPost] = useState([
+        { id: 1, title: "Post 1", body: "This is post 1 This is post 1 This is post 1" },
+        { id: 2, title: "Post 2", body: "This is post 2 This is post 1 This is post 1" },
+        { id: 3, title: "Post 3", body: "This is post 3 This is post 1 This is post 1" },
     ]);
 
-    const handleDelete =(id) => {
+    const handleDelete = (id) => {
 
         setPost(post.filter(post => post.id !== id));
     }
 
-    
-    return ( <div>
+
+    return (<div>
         <h1>Blog Post</h1>
 
-        <ul>
-            {post.map((post) => {
-                <li key={post.id}>
-                    <Link to={'/post/${post.id}'}>{post.id}</Link>
-                    <button onClick={handleDelete(post.id)}>Delete</button>
+        <ul className="blogList">
+            {post.map((post) => (
+                <li className="item-container" key={post.id}>
+                    <Link  className="item" to={`/post/${post.id}`}>{post.body}</Link>
+                    <button onClick={() => handleDelete(post.id)}>Delete</button>
                 </li>
-            })}
+            ))}
         </ul>
-    </div> );
+    </div>);
 }
- 
+
 export default Home;
