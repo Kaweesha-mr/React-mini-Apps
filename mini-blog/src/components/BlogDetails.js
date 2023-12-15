ab1ee86ab8baa765df579cf409367f94715b6e3c
= <kaweesha.mr@gmail.com>
2023-12-15 13:32:36 +0530

    Add blog details component

--- mini-blog/src/components/BlogDetails.js
+++ mini-blog/src/components/BlogDetails.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+const BlogDetails = ({match,blogs}) => {
+    const blogId = match.params.id;
+
+    //this will find and return the blog with the id that matches the id in the url
+    const blog = blogs.find(blog => blog.id === blogId);
+    
+    return ( 
+
+        <div className="blog-details">
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <div>{blog.content}</div>
+        </div>
+
+     );
+}
+ 
+export default BlogDetails;
