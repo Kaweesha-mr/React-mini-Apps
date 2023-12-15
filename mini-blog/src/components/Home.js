6123d7320495896ae536a6c1e51cdd850e98da8f
= <kaweesha.mr@gmail.com>
2023-12-15 12:15:41 +0530

    Add dynamic blog list and add blog link to Home component

--- mini-blog/src/components/Home.js
+++ mini-blog/src/components/Home.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+    //usestate is going to use here because we have to manage the state of the blog 
+    //use to manage dynamic data becouse blogs can be deleted or added
+
+    const [blogs,setBlogs] = useState([
+        {id:1, title: 'Blog 1', content: 'content for Blog 1'},
+        {id:2, title: 'Blog 2', content: 'content for Blog 2'},
+    ])
     return ( 
         <div>
             <h2>Welcome to the Blog Page</h2>
             {/* display the currentli available blog pages */}
+
+            <ul>
+                    {/* insted of hardcoding we are going to dynamiclly change these things */}
+                    
+                    {blogs.map( blog => (
+
+                        <li key={blog.id}>
+                            <link to={`blog/${blog.id}`}>{blog.title}</link>
+                        </li>    
+                    ))}
+            </ul>
+
+            <Link to="/add-blog">Add a New Blog</Link>
         </div>
      );
 }
- 
 export default Home;
