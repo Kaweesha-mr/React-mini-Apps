41ce29b655b96b48ab8624198323adec5cc808e2
= <kaweesha.mr@gmail.com>
2023-12-15 13:18:37 +0530

    Add useState hook and form handling in AddBlog component

--- mini-blog/src/components/AddBlog.js
+++ mini-blog/src/components/AddBlog.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const AddBlog = () => {
+    
+    const [title,setTitle] = useState('');
+    const [content,setContent] = useState('');
+
+    //when the submit button clicks there should be a function to handle it
+
+    const handleAddBlog = () => {
+
+    }
+
+
     return ( 
         <div>
             <h2>Add a new Blog</h2>
-            {/* form elemets will go here */}
+            <form>
+                <label>Title:</label>
+                <input
+                    type="text"
+                    value={title}
+                    onChange={e=> setContent(e.target.value)} />
+                <label>Content:</label>
+                <textarea
+                    value={content}
+                    onChange={e=> setContent(e.target.value)} />
+                
+                <button onClick={handleAddBlog}>Add Blog</button>
+            </form>
         </div>
      );
 }
